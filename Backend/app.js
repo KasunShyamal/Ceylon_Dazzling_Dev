0c6456588b8dc231874101185b33b4373860f11b
KasunShyamal <kshyamal48@gmail.com>
2023-11-28 15:17:06 +0530

    CRUD blog

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,19 @@
 import express from "express";
 import mongoose from "mongoose";
 import router from "./routes/user-routes";
+import blogRouter from "./routes/blog-routes";
 
 
 const app = express();
 
 app.use(express.json());
 app.use("/api/user", router);
+app.use("/api/blog", blogRouter);
+
+
+
+
+
 mongoose.connect("mongodb+srv://kshyamal:8GjWSxPcJlI4a5U4@cluster0.bfgpdp7.mongodb.net/BlogApp?retryWrites=true&w=majority")
 .then(()=>app.listen(5000))
 .then(()=>console.log("DB Connected"))
--- Backend/controllers/user-controller.js
+++ Backend/controllers/user-controller.js
@@ -1,4 +1,5 @@
 import User from "../model/User";
+import bcrypt from 'bcryptjs';
 
 export const getAllUsers = async (req, res, next) => {
     let users;
@@ -25,10 +26,12 @@ export const signup = async (req, res, next) => {
     if(existingUser) {
         return res.status(400).json({message: "User Already Exists"});
     }
+    const hashPassword = bcrypt.hashSync(password);
+
     const user = new User({
         name,
         email,
-        password
+        password: hashPassword
     });
 
     try {
@@ -37,4 +40,24 @@ export const signup = async (req, res, next) => {
         return console.log(err);
     }
     return res.status(201).json({ user });
+};
+
+export const login = async(req, res, next) => {
+    const { email, password }  = req.body;
+    let existingUser;
+    try {
+        existingUser = await User.findOne({ email });
+    } catch (err) {
+        return console.log(err);
+    }
+    if(!existingUser) {
+        return res.status(404).json({message: "Could not find the user"});
+    }
+
+    const isPasswordcorrect = bcrypt.compareSync(password, existingUser.password);
+    if(!isPasswordcorrect){
+        return res.status(400).json({message:"Password Incorrect"});
+    }
+    return res.status(200).json({message: "Login Success"});
+
 };
